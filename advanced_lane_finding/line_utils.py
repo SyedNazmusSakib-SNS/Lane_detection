2ce6b094be98ec40a6cc57d516c8e84ba8a8e0c8
Syed Nazmus Sakib <syednazmussakib64@gmail.com>
2024-05-18 00:00:47 +0600

    Your commit message

---
+++ advanced_lane_finding/line_utils.py
@@ -0,0 +1,279 @@
+import numpy as np
+import cv2
+import glob
+import collections
+import matplotlib.pyplot as plt
+from calibration_utils import calibrate_camera, undistort
+from binarization_utils import binarize
+from perspective_utils import birdeye
+from globals import ym_per_pix, xm_per_pix
+
+class Line:
+    """
+    Class to model a lane-line.
+    """
+    def __init__(self, buffer_len=10):
+        # flag to mark if the line was detected the last iteration
+        self.detected = False
+        # polynomial coefficients fitted on the last iteration
+        self.last_fit_pixel = None
+        self.last_fit_meter = None
+        # list of polynomial coefficients of the last N iterations
+        self.recent_fits_pixel = collections.deque(maxlen=buffer_len)
+        self.recent_fits_meter = collections.deque(maxlen=2 * buffer_len)
+        self.radius_of_curvature = None
+        # store all pixels coords (x, y) of line detected
+        self.all_x = None
+        self.all_y = None
+
+    def update_line(self, new_fit_pixel, new_fit_meter, detected, clear_buffer=False):
+        """
+        Update Line with new fitted coefficients.
+        """
+        self.detected = detected
+        if clear_buffer:
+            self.recent_fits_pixel.clear()
+            self.recent_fits_meter.clear()
+        self.last_fit_pixel = new_fit_pixel
+        self.last_fit_meter = new_fit_meter
+        self.recent_fits_pixel.append(self.last_fit_pixel)
+        self.recent_fits_meter.append(self.last_fit_meter)
+
+    def draw(self, mask, color=(255, 0, 0), line_width=50, average=False):
+        """
+        Draw the line on a color mask image.
+        """
+        h, w, c = mask.shape
+        plot_y = np.linspace(0, h - 1, h)
+        coeffs = self.average_fit if average else self.last_fit_pixel
+        line_center = coeffs[0] * plot_y ** 2 + coeffs[1] * plot_y + coeffs[2]
+        line_left_side = line_center - line_width // 2
+        line_right_side = line_center + line_width // 2
+        pts_left = np.array(list(zip(line_left_side, plot_y)))
+        pts_right = np.array(np.flipud(list(zip(line_right_side, plot_y))))
+        pts = np.vstack([pts_left, pts_right])
+        return cv2.fillPoly(mask, [np.int32(pts)], color)
+
+    @property
+    def average_fit(self):
+        """
+        Average of polynomial coefficients of the last N iterations.
+        """
+        return np.mean(self.recent_fits_pixel, axis=0)
+
+    @property
+    def curvature(self):
+        """
+        Radius of curvature of the line (averaged).
+        """
+        y_eval = 0
+        coeffs = self.average_fit
+        return ((1 + (2 * coeffs[0] * y_eval + coeffs[1]) ** 2) ** 1.5) / np.absolute(2 * coeffs[0])
+
+    @property
+    def curvature_meter(self):
+        """
+        Radius of curvature of the line (averaged) in meters.
+        """
+        y_eval = 0
+        coeffs = np.mean(self.recent_fits_meter, axis=0)
+        return ((1 + (2 * coeffs[0] * y_eval + coeffs[1]) ** 2) ** 1.5) / np.absolute(2 * coeffs[0])
+
+def get_fits_by_sliding_windows(birdeye_binary, line_lt, line_rt, n_windows=9, verbose=False):
+    """
+    Get polynomial coefficients for lane-lines detected in a binary image.
+    """
+    height, width = birdeye_binary.shape
+    histogram = np.sum(birdeye_binary[height//2:-30, :], axis=0)
+    out_img = np.dstack((birdeye_binary, birdeye_binary, birdeye_binary)) * 255
+    midpoint = len(histogram) // 2
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    window_height = int(height / n_windows)
+    nonzero = birdeye_binary.nonzero()
+    nonzero_y = np.array(nonzero[0])
+    nonzero_x = np.array(nonzero[1])
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    margin = 100
+    minpix = 50
+    left_lane_inds = []
+    right_lane_inds = []
+
+    for window in range(n_windows):
+        win_y_low = height - (window + 1) * window_height
+        win_y_high = height - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
+        good_left_inds = ((nonzero_y >= win_y_low) & (nonzero_y < win_y_high) & (nonzero_x >= win_xleft_low) & (nonzero_x < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzero_y >= win_y_low) & (nonzero_y < win_y_high) & (nonzero_x >= win_xright_low) & (nonzero_x < win_xright_high)).nonzero()[0]
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+        if len(good_left_inds) > minpix:
+            leftx_current = int(np.mean(nonzero_x[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = int(np.mean(nonzero_x[good_right_inds]))
+
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+    line_lt.all_x, line_lt.all_y = nonzero_x[left_lane_inds], nonzero_y[left_lane_inds]
+    line_rt.all_x, line_rt.all_y = nonzero_x[right_lane_inds], nonzero_y[right_lane_inds]
+
+    detected = True
+    if not list(line_lt.all_x) or not list(line_lt.all_y):
+        left_fit_pixel = line_lt.last_fit_pixel
+        left_fit_meter = line_lt.last_fit_meter
+        detected = False
+    else:
+        left_fit_pixel = np.polyfit(line_lt.all_y, line_lt.all_x, 2)
+        left_fit_meter = np.polyfit(line_lt.all_y * ym_per_pix, line_lt.all_x * xm_per_pix, 2)
+
+    if not list(line_rt.all_x) or not list(line_rt.all_y):
+        right_fit_pixel = line_rt.last_fit_pixel
+        right_fit_meter = line_rt.last_fit_meter
+        detected = False
+    else:
+        right_fit_pixel = np.polyfit(line_rt.all_y, line_rt.all_x, 2)
+        right_fit_meter = np.polyfit(line_rt.all_y * ym_per_pix, line_rt.all_x * xm_per_pix, 2)
+
+    line_lt.update_line(left_fit_pixel, left_fit_meter, detected=detected)
+    line_rt.update_line(right_fit_pixel, right_fit_meter, detected=detected)
+
+    ploty = np.linspace(0, height - 1, height)
+    left_fitx = left_fit_pixel[0] * ploty ** 2 + left_fit_pixel[1] * ploty + left_fit_pixel[2]
+    right_fitx = right_fit_pixel[0] * ploty ** 2 + right_fit_pixel[1] * ploty + right_fit_pixel[2]
+    out_img[nonzero_y[left_lane_inds], nonzero_x[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzero_y[right_lane_inds], nonzero_x[right_lane_inds]] = [0, 0, 255]
+
+    if verbose:
+        f, ax = plt.subplots(1, 2)
+        f.set_facecolor('white')
+        ax[0].imshow(birdeye_binary, cmap='gray')
+        ax[1].imshow(out_img)
+        ax[1].plot(left_fitx, ploty, color='yellow')
+        ax[1].plot(right_fitx, ploty, color='yellow')
+        ax[1].set_xlim(0, 1280)
+        ax[1].set_ylim(720, 0)
+        plt.show()
+
+    return line_lt, line_rt, out_img
+
+def get_fits_by_previous_fits(birdeye_binary, line_lt, line_rt, verbose=False):
+    height, width = birdeye_binary.shape
+    left_fit_pixel = line_lt.last_fit_pixel
+    right_fit_pixel = line_rt.last_fit_pixel
+    nonzero = birdeye_binary.nonzero()
+    nonzero_y = np.array(nonzero[0])
+    nonzero_x = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds = ((nonzero_x > (left_fit_pixel[0] * (nonzero_y ** 2) + left_fit_pixel[1] * nonzero_y + left_fit_pixel[2] - margin)) & (nonzero_x < (left_fit_pixel[0] * (nonzero_y ** 2) + left_fit_pixel[1] * nonzero_y + left_fit_pixel[2] + margin)))
+    right_lane_inds = ((nonzero_x > (right_fit_pixel[0] * (nonzero_y ** 2) + right_fit_pixel[1] * nonzero_y + right_fit_pixel[2] - margin)) & (nonzero_x < (right_fit_pixel[0] * (nonzero_y ** 2) + right_fit_pixel[1] * nonzero_y + right_fit_pixel[2] + margin)))
+
+    line_lt.all_x, line_lt.all_y = nonzero_x[left_lane_inds], nonzero_y[left_lane_inds]
+    line_rt.all_x, line_rt.all_y = nonzero_x[right_lane_inds], nonzero_y[right_lane_inds]
+
+    detected = True
+    if not list(line_lt.all_x) or not list(line_lt.all_y):
+        left_fit_pixel = line_lt.last_fit_pixel
+        left_fit_meter = line_lt.last_fit_meter
+        detected = False
+    else:
+        left_fit_pixel = np.polyfit(line_lt.all_y, line_lt.all_x, 2)
+        left_fit_meter = np.polyfit(line_lt.all_y * ym_per_pix, line_lt.all_x * xm_per_pix, 2)
+
+    if not list(line_rt.all_x) or not list(line_rt.all_y):
+        right_fit_pixel = line_rt.last_fit_pixel
+        right_fit_meter = line_rt.last_fit_meter
+        detected = False
+    else:
+        right_fit_pixel = np.polyfit(line_rt.all_y, line_rt.all_x, 2)
+        right_fit_meter = np.polyfit(line_rt.all_y * ym_per_pix, line_rt.all_x * xm_per_pix, 2)
+
+    line_lt.update_line(left_fit_pixel, left_fit_meter, detected=detected)
+    line_rt.update_line(right_fit_pixel, right_fit_meter, detected=detected)
+
+    ploty = np.linspace(0, height - 1, height)
+    left_fitx = left_fit_pixel[0] * ploty ** 2 + left_fit_pixel[1] * ploty + left_fit_pixel[2]
+    right_fitx = right_fit_pixel[0] * ploty ** 2 + right_fit_pixel[1] * ploty + right_fit_pixel[2]
+
+    img_fit = np.dstack((birdeye_binary, birdeye_binary, birdeye_binary)) * 255
+    window_img = np.zeros_like(img_fit)
+
+    img_fit[nonzero_y[left_lane_inds], nonzero_x[left_lane_inds]] = [255, 0, 0]
+    img_fit[nonzero_y[right_lane_inds], nonzero_x[right_lane_inds]] = [0, 0, 255]
+
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+    result = cv2.addWeighted(img_fit, 1, window_img, 0.3, 0)
+
+    if verbose:
+        plt.imshow(result)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+        plt.show()
+
+    return line_lt, line_rt, img_fit
+
+def draw_back_onto_the_road(img_undistorted, Minv, line_lt, line_rt, keep_state):
+    height, width, _ = img_undistorted.shape
+
+    left_fit = line_lt.average_fit if keep_state else line_lt.last_fit_pixel
+    right_fit = line_rt.average_fit if keep_state else line_rt.last_fit_pixel
+
+    ploty = np.linspace(0, height - 1, height)
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+    road_warp = np.zeros_like(img_undistorted, dtype=np.uint8)
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+    cv2.fillPoly(road_warp, np.int_([pts]), (0, 255, 0))
+    road_dewarped = cv2.warpPerspective(road_warp, Minv, (width, height))
+
+    blend_onto_road = cv2.addWeighted(img_undistorted, 1., road_dewarped, 0.3, 0)
+
+    line_warp = np.zeros_like(img_undistorted)
+    line_warp = line_lt.draw(line_warp, color=(255, 0, 0), average=keep_state)
+    line_warp = line_rt.draw(line_warp, color=(0, 0, 255), average=keep_state)
+    line_dewarped = cv2.warpPerspective(line_warp, Minv, (width, height))
+
+    lines_mask = blend_onto_road.copy()
+    idx = np.any([line_dewarped != 0][0], axis=2)
+    lines_mask[idx] = line_dewarped[idx]
+
+    blend_onto_road = cv2.addWeighted(src1=lines_mask, alpha=0.8, src2=blend_onto_road, beta=0.5, gamma=0.)
+
+    return blend_onto_road
+
+if __name__ == '__main__':
+
+    line_lt, line_rt = Line(buffer_len=10), Line(buffer_len=10)
+
+    ret, mtx, dist, rvecs, tvecs = calibrate_camera(calib_images_dir='camera_cal')
+
+    for test_img in glob.glob('test_images/*.jpg'):
+
+        img = cv2.imread(test_img)
+
+        img_undistorted = undistort(img, mtx, dist, verbose=False)
+
+        img_binary = binarize(img_undistorted, verbose=False)
+
+        img_birdeye, M, Minv = birdeye(img_binary, verbose=False)
+
+        line_lt, line_rt, img_out = get_fits_by_sliding_windows(img_birdeye, line_lt, line_rt, n_windows=7, verbose=True)
